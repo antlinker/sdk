3d0b8a294b29040f6a9d4652fb00de38a9d3e5c4
lyric <tiannianshou@gmail.com>
2017-09-14 11:14:08 +0800

    增加ats

---
+++ ats/README.md
@@ -0,0 +1,7 @@
+# 工具化服务SDK
+
+## 初始化
+
+``` go
+ats.SetConfig(&ats.Config{HTTPAddr:"http://127.0.0.1:9980"})
+```
---
+++ ats/ats.go
@@ -0,0 +1,77 @@
+package ats
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/antlinker/sdk/utils"
+)
+
+var (
+	config *Config
+)
+
+// Config 配置参数
+type Config struct {
+	HTTPAddr string
+}
+
+// SetConfig 初始化
+func SetConfig(cfg *Config) {
+	config = cfg
+}
+
+// ConvertHTMLToPDF html转换为pdf
+func ConvertHTMLToPDF(src []byte) ([]byte, error) {
+	if config == nil {
+		return nil, nil
+	}
+
+	addr := config.HTTPAddr
+	if len(addr) == 0 {
+		return nil, nil
+	} else if addr[len(addr)-1] == '/' {
+		addr = addr[:len(addr)-1]
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/api/pdf", addr), bytes.NewBuffer(src))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+
+	var pdfData []byte
+	err = utils.Request(ctx, req, func(resp *http.Response, err error) error {
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		zr, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		buf, err := ioutil.ReadAll(zr)
+		if err != nil {
+			return err
+		}
+		pdfData = buf
+
+		zr.Close()
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return pdfData, nil
+}
