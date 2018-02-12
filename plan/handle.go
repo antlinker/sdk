f65c376c17a63649429fb8fa047f3c7beb41af8b
ybq <yinbaoqiang@antlinker.com>
2018-02-12 09:59:03 +0800

    新增plan sdk

---
+++ plan/handle.go
@@ -0,0 +1,126 @@
+package plan
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/antlinker/sdk/asapi"
+	"github.com/antlinker/sdk/utils"
+)
+
+const (
+	jobRouter = "/job/plan"
+)
+
+// Config 配置参数
+type Config struct {
+	HTTPAddr string
+}
+
+// NewHandle 创建作业任务
+func NewHandle(auh *asapi.AuthorizeHandle, config *Config) *Handle {
+	return &Handle{
+		auh: auh,
+		cfg: config,
+	}
+}
+
+// Handle 作业任务
+type Handle struct {
+	auh *asapi.AuthorizeHandle
+	cfg *Config
+}
+
+func (h *Handle) getURL(router string) string {
+	addr := h.cfg.HTTPAddr
+	if len(addr) == 0 {
+		return ""
+	}
+
+	if addr[len(addr)-1] == '/' {
+		addr = addr[:len(addr)-1]
+	}
+
+	return addr + router
+}
+
+func (h *Handle) getANTUserID(intelUserCode string) (userID string, err error) {
+	auids, ar := h.auh.GetAntUIDList("", intelUserCode)
+	if ar != nil {
+		err = ar
+		return
+	} else if len(auids) == 0 {
+		err = errors.New("not found user")
+		return
+	}
+	userID = auids[0]
+	return
+}
+// Request 计划请求
+type Request struct {
+	Type      string    `json:"typ"`
+	Data      string    `json:"data"`
+	StartTime time.Time `json:"startTime"`
+	Spec      string    `json:"spec"`
+	Repeat    int       `json:"repeat"`
+}
+func (h *Handle) request(req Request) (err error) {
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+
+	defer cancel()
+
+	
+	
+	data, err := utils.PostJSON(ctx, h.getURL(jobRouter), req, func(req *http.Request) (*http.Request, error) {
+		token, err := asapi.GetToken()
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("AccessToken", token)
+		return req, nil
+	})
+	if err != nil {
+		if len(data) > 0 {
+			log.Println("请求发生错误：", string(data))
+		}
+		return
+	}
+
+	var str string
+	json.Unmarshal(data, &str)
+	if str != "ok" {
+		err = fmt.Errorf(string(data))
+	}
+
+	return
+}
+
+// Test 测试任务
+func (h *Handle) Test(spec string,startTime time.Time,repeat int) (err error) {
+	
+	
+	err = h.AddPlan(Request {
+		Type:"test",
+		Spec:spec,
+		StartTime:startTime,
+		Repeat:repeat,
+
+	})
+
+	return
+}
+
+// AddPlan 添加任务
+func (h *Handle) AddPlan( req Request) error{
+	if req.StartTime.IsZero() {
+		req.StartTime=time.Now()
+	}
+	return h.request(req)
+}
---
+++ plan/plan_test.go
@@ -0,0 +1,28 @@
+package plan_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/antlinker/sdk/asapi"
+	"github.com/antlinker/sdk/plan"
+)
+
+func TestTest(t *testing.T) {
+	asapi.InitAPI(&asapi.Config{
+		ASURL:           "http://192.168.1.202:8090",
+		ClientID:        "57d0c56597d5b1413bd01262",
+		ClientSecret:    "0d1414f8743294f14c53cb46bcb8d5ea3e773026",
+		ServiceIdentify: "ANT",
+		IsEnabledCache:  true,
+		CacheGCInterval: 60,
+	})
+	plan.SetConfig(&plan.Config{
+		HTTPAddr: "http://127.0.0.1:8901",
+	})
+	err:=plan.Test("", time.Now().Add(10*time.Second), 1)
+	if err!=nil {
+		t.Error(err)
+	}
+
+}
