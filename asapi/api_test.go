38757c9f06c64ee586396565e6e5f2155510a729
wuqingtao <wuqingtao@antlinker.com>
2020-02-10 17:29:02 +0800

    修改GetUserCode方法里的body为实现了RequestReader的结构体

--- asapi/api_test.go
+++ asapi/api_test.go
@@ -4,10 +4,10 @@ var gconfig *Config
 
 func init() {
 	gconfig = &Config{
-		ASURL:           "https://127.0.0.1:8090",
-		ClientID:        "5e3e73941d41c83e2f0321b2",
-		ClientSecret:    "bcd75fbea088359421ab38f790a7143e5e3243cd",
-		ServiceIdentify: "ANT",
+		ASURL:           "",
+		ClientID:        "",
+		ClientSecret:    "",
+		ServiceIdentify: "",
 		IsEnabledCache:  true,
 	}
 }
